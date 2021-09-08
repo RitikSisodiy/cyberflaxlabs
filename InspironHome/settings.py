22babde3b31fa1e3588a22796fca590d2ecb4ed2
RitikSisodiy <ritik.s10120@gmail.com>
2021-09-09 01:27:09 +0530

    add share links

--- InspironHome/settings.py
+++ InspironHome/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'InspironHome.urls'
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
